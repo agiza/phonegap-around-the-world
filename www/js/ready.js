3ac9bd83358bf29eff94babcf4ccde89553fe2f1
Andrew Chilton <andychilton@gmail.com>
2012-10-29 11:58:11 +1300

    Add some stats to the compass

--- www/js/ready.js
+++ www/js/ready.js
@@ -9,6 +9,65 @@ function log(msg) {
     }
 }
 
+var stats = [
+    {
+        city : 'Beijing',
+        population : '19,612,368',
+        area : '16,801.25',
+        country : 'People\'s Republic of China',
+        density : '1,200'
+    },
+    {
+        city : 'Helsinki',
+        population : '602,200',
+        area : '715.49',
+        country : 'Finland',
+        density : '1,391.13'
+    },
+    {
+        city : 'Liverpool',
+        population : '466,400',
+        area : '43.18',
+        country : 'United Kingdom',
+        density : '10,070'
+    },
+    {
+        city : 'Reykjavík',
+        population : '119,108',
+        area : '777',
+        country : 'Iceland',
+        density : '436.5'
+    },
+    {
+        city : 'São Paulo',
+        population : '11,316,149',
+        area : '1,522.986',
+        country : 'Brazil',
+        density : '7,216.3'
+    },
+    {
+        city : 'Cape Town',
+        population : '827,218',
+        area : '496.70',
+        country : 'South Africa',
+        density : '1,700'
+    },
+    {
+        city : 'Boston',
+        population : '625,087',
+        area : '89.63',
+        country : 'United States',
+        density : '12,752'
+    },
+    {
+        city : 'Tokyo',
+        population : '13,185,502',
+        area : '2,187.66',
+        country : 'Japan',
+        density : '6,000'
+    }
+];
+
 function deviceReady() {
     log('deviceReady(): entry');
 
@@ -28,6 +87,13 @@ function deviceReady() {
             $city.text(angle);
             $angle.text(angle + '°');
         }
+
+        var stat = parseInt(angle/45);
+        $('#stat-city').text(stats[stat].city);
+        $('#stat-population').text(stats[stat].population);
+        $('#stat-area').text(stats[stat].area);
+        $('#stat-country').text(stats[stat].country);
+        $('#stat-density').text(stats[stat].density);
     }
     function noop() {}
 
